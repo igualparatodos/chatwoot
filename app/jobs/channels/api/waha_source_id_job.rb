9f1f6c809ee081470a485a761362282cd866b576
Kenji <kenji@igual.com>
2024-12-18 10:39:30 -0300

    fix

--- app/jobs/channels/api/waha_source_id_job.rb
+++ app/jobs/channels/api/waha_source_id_job.rb
@@ -5,23 +5,18 @@ class Channels::Api::WahaSourceIdJob < ApplicationJob
   MAX_RETRIES = 5
 
   def perform(record_id, retry_count = 0)
+    Rails.logger.info "Job to check waha message status for #{record_id}"
+
     message = Message.find(record_id)
 
     if retry_count >= MAX_RETRIES
       message.update!(status: :failed)
-      conversation = message.conversation
-      params = {
-        private: true,
-        content: "Erro, mensagem não enviada",
-        in_reply_to: record_id
-      }
-      Messages::MessageBuilder.new(conversation.user, conversation, params).perform
-
       Rails.logger.error "Job failed after #{MAX_RETRIES} retries for record ID #{record_id}"
       return
     end
 
     unless message.source_id.present?
+      Rails.logger.info "Retrying job for record ID #{record_id}, attempt #{retry_count + 1}"
       self.class.set(wait: 5.seconds).perform_later(record_id, retry_count + 1)
     end
   end
